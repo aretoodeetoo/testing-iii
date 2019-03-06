bfbe5f95f67df81c08e99875407623604a397ee3
croy <roy.christie95@gmail.com>
2019-03-06 15:47:25 -0600

    Added test to ensure lock button renders

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('Control panel', () => {
+    it('renders lock button', () => {
+        const { getByText } = render(<Controls />);
+        const lockButton = getByText(/lock gate/i);  
+        expect(lockButton).toHaveTextContent(/lock gate/i);
+    })
+});
